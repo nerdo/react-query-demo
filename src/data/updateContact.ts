417f89afbcf802ae3d91a234db6bb492196ea638
Dannel Albert <cuebix@gmail.com>
2022-09-27 08:27:20 -0400

    refactor: remove updateContact.ts

--- src/data/updateContact.ts
+++
@@ -1,8 +0,0 @@
-import { Contact, ContactUpdate, db } from './db'
-
-export const updateContact = async (props: ContactUpdate) => {
-  const model = await db.updateContact(props)
-  return model.attrs as Contact
-}
-
-export default updateContact
